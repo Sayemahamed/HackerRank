462852fc92b0e7cb65dc2561f761aa305a5b2d76
Sayemahamed <sayemahamed183@gmail.com>
2024-09-06 23:09:59 +0600

    * is used before the name of variable to take list input of dynamic size

---
+++ Finding_the_percentage.py
@@ -0,0 +1,12 @@
+def main():
+    number_inputs = int(input().strip())
+    student_marks = {}
+    for _ in range(number_inputs):
+        # * is used to take input as a list
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input().strip()
+    print("{:.2f}".format(sum(student_marks[query_name])/student_marks[query_name].__len__()))
+if __name__ == '__main__':
+    main()
