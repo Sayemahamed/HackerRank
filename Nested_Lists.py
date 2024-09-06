383cd3348dae06de8c047992ea03f3e0f17a106f
Sayemahamed <sayemahamed183@gmail.com>
2024-09-06 22:57:37 +0600

    values needs to be initialized first in key value pair

---
+++ Nested_Lists.py
@@ -0,0 +1,16 @@
+def main():
+    student_number=int(input())
+    student_marks={}
+    for _ in range(student_number):
+        name = input()
+        score = float(input().strip())
+        if score not in student_marks:
+            student_marks[score]=[name]
+        else:
+            student_marks[score].append(name)
+    second_lowest=sorted(student_marks.keys())[1]
+    student_marks[second_lowest].sort() 
+    print('\n'.join(student_marks[second_lowest]))
+      
+if __name__ == '__main__':
+    main()
