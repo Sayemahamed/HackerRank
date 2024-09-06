6813759f56b9e5577e96ab865da1ae80b696ca1e
Sayemahamed <sayemahamed183@gmail.com>
2024-09-06 17:41:08 +0600

    third argument is for mod in pow

---
+++ Power_Mod_Power.py
@@ -0,0 +1,9 @@
+def main():
+    a = int(input().strip())
+    b = int(input().strip())
+    c = int(input().strip())
+    print(pow(a,b))
+    print(pow(a,b,c))
+
+if __name__ == '__main__':
+    main()
