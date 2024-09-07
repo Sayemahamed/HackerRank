996298b9fefd0c45e15a993ac85019f0b5ada128
Sayemahamed <sayemahamed183@gmail.com>
2024-09-07 22:59:38 +0600

    reformated with black and completed number conversion

--- Designer_Door_Mat.py
+++ Designer_Door_Mat.py
@@ -1,10 +1,10 @@
 def main():
-    x,y = map(int,input().strip().split())
-    for i in range(1,x,2):
-        print((".|."*i).center(y,"-" ))
-    print("WELCOME".center(y,"-"))
-    for i in range(x-2,0,-2):
-        print((".|."*i).center(y,"-"))
+    x, y = map(int, input().strip().split())
+    for i in range(1, x, 2):
+        print((".|." * i).center(y, "-"))
+    print("WELCOME".center(y, "-"))
+    for i in range(x - 2, 0, -2):
+        print((".|." * i).center(y, "-"))
 
 
 if __name__ == "__main__":
