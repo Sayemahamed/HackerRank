afe534602c305f275bd1d3bac4e0f0aa4d02f6d6
Sayemahamed <sayemahamed183@gmail.com>
2024-09-08 18:12:34 +0600

    basic set operations

--- Set_difference_Operation.py
+++ Set_difference_Operation.py
@@ -3,8 +3,8 @@ def main():
     set_a = set(map(int, input().strip().split()))
     set_b_count = int(input().strip())
     set_b = set(map(int, input().strip().split()))
-    ans = set_a.intersection(set_b)
-    print(len(set_a)-len(ans))
+    ans = set_a.difference(set_b)
+    print(len(ans))
 
 
 if __name__ == "__main__":
